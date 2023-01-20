5468ec0577c83a610103e4b50854947a1a600a8d
tchen4 <tchen4@uw.edu>
2023-01-20 11:54:38 -0800

    Webpage changes

--- m1/content-scripts/replace.js
+++ m1/content-scripts/replace.js
@@ -3,15 +3,4 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const newFont = message.font;
 
   document.body.style.fontFamily = newFont;
-
-  /*function (font) {
-    document.body.style.fontFamily = font.value;
-  }*/
-  /*changeText();*/
 });
-
-// Checks the current value of replace and run walk nodes
-function changeText() {
-  alert("hello!")
-  document.body.style.fontFamily = "monospace";
-}
--- m1/popup/popup.js
+++ m1/popup/popup.js
@@ -1,31 +1,3 @@
-/*const addBlockButton = document.getElementById("open-options");
-
-// Add event listeners to the checkbox and button
-addBlockButton.addEventListener("click", (e) => updateContentScript(true));*/
-
-/*async function updateContentScript() {
-  // Sends a message to the content script with an object that has the
-  // current value of the checkbox and a boolean (whether to add a block)
-  const message = { message: "Change the text" };
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
-  const response = await chrome.tabs.sendMessage(tab.id, message);
-  // You can do something with response from the content script here
-  console.log(response);
-}
-
-const messageButton = document.getElementById("open-options");
-const fontPicker = document.getElementById("input-font");
-
-const sendMessage = (e) => {
-  updateContentScript({ font: fontPicker.value })
-};
-
-messageButton.addEventListener("click", (e) => updateContentScript(true));*/
-
-
 async function sendMessageToContentScript(message) {
   const [tab] = await chrome.tabs.query({
     active: true,
